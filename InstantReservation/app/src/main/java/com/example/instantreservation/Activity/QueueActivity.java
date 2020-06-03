228f5abd52fc19bf34b7c8d5f20de4615df170de
Gioele <gcageggi@gmail.com>
2020-06-03 13:12:37 +0200

    Retrieve data from firebase with queue info + construct card with info catched

--- InstantReservation/app/src/main/java/com/example/instantreservation/Activity/QueueActivity.java
+++ InstantReservation/app/src/main/java/com/example/instantreservation/Activity/QueueActivity.java
@@ -7,8 +7,10 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.instantreservation.MyDoes;
 import com.example.instantreservation.Queue;
 import com.example.instantreservation.R;
 import com.google.firebase.database.DataSnapshot;
@@ -21,27 +23,53 @@ import org.json.JSONObject;
 
 public class QueueActivity extends AppCompatActivity {
 
-    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference reference;
+    TextView queue_name;
+    TextView queue_business;
+    TextView queue_city;
+    TextView queue_nReservation;
+    TextView queue_description ;
+    ImageView queue_QRCodeImage;
+    ImageView queue_image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_queue);
 
-        TextView queue_id = findViewById(R.id.queue_id);
+        queue_business = findViewById(R.id.queue_business);
+        queue_name = findViewById(R.id.queue_name);
+        queue_city = findViewById(R.id.queue_city);
+        queue_nReservation = findViewById(R.id.queue_nReservation);
+        queue_description = findViewById(R.id.queue_description);
+        queue_QRCodeImage = findViewById(R.id.queue_QRCodeImage);
+        queue_image = findViewById(R.id.queue_image);
+
         Intent intent = getIntent();
-        String id = intent.getStringExtra("payload");
-        queue_id.setText(id);
+        final String id = intent.getStringExtra("payload");
 
-        DatabaseReference ref = database.getReference("instant-reservation-6c2a2/codeattivita/"+id);
-        // Attach a listener to read the data at our posts reference
-        ref.addValueEventListener(new ValueEventListener() {
+        reference = FirebaseDatabase.getInstance().getReference().child("codeattivita");
+
+        reference.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Queue queue = dataSnapshot.getValue(Queue.class);
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Queue queue = dataSnapshot.child(id).getValue(Queue.class);
+                queue_business.setText(queue.getQueue_business());
+                queue_city.setText(queue.getQueue_city());
+                queue_description.setText(queue.getQueue_description());
+                queue_name.setText(queue.getQueue_name());
+                queue_nReservation.setText(queue.getQueue_nReservationString());
+                //queue_image.setImageURI(queue.getQueue_image());
+                //queue_QRCodeImage.setImageURI();
 
-                System.out.println("AaaaaAAAAAAAAAAAAAAAAAAAAAAAAA" + dataSnapshot.getValue());
+        }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
+        });
+
 
     }
 
@@ -55,6 +83,5 @@ public class QueueActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
 
 }
--- InstantReservation/app/src/main/java/com/example/instantreservation/Queue.java
+++ InstantReservation/app/src/main/java/com/example/instantreservation/Queue.java
@@ -1,18 +1,26 @@
 package com.example.instantreservation;
 
 public class Queue {
-    final String queue_name;
-    final String queue_description;
-    final String queue_business;
-    final int queue_nMaxReservation;
-    final int queue_nReservation;
+    String queue_name;
+    String queue_description;
+    String queue_business;
+    String queue_QRCodeImage;
+    String queue_image;
+    String queue_city;
+    int queue_nMaxReservation;
+    int queue_nReservation;
 
+    public Queue(){
 
+    }
 
-    public Queue(String queue_name, String queue_description, String queue_business, int queue_nMaxReservation, int queue_nReservation) {
+    public Queue(String queue_name, String queue_description, String queue_business, String queue_QRCodeImage, String queue_image, String queue_city, int queue_nMaxReservation, int queue_nReservation) {
         this.queue_name = queue_name;
         this.queue_description = queue_description;
         this.queue_business = queue_business;
+        this.queue_QRCodeImage = queue_QRCodeImage;
+        this.queue_image = queue_image;
+        this.queue_city = queue_city;
         this.queue_nMaxReservation = queue_nMaxReservation;
         this.queue_nReservation = queue_nReservation;
     }
@@ -21,19 +29,67 @@ public class Queue {
         return queue_name;
     }
 
+    public void setQueue_name(String queue_name) {
+        this.queue_name = queue_name;
+    }
+
     public String getQueue_description() {
         return queue_description;
+    }
+
+    public void setQueue_description(String queue_description) {
+        this.queue_description = queue_description;
     }
 
     public String getQueue_business() {
         return queue_business;
     }
 
+    public void setQueue_business(String queue_business) {
+        this.queue_business = queue_business;
+    }
+
+    public String getQueue_QRCodeImage() {
+        return queue_QRCodeImage;
+    }
+
+    public void setQueue_QRCodeImage(String queue_QRCodeImage) {
+        this.queue_QRCodeImage = queue_QRCodeImage;
+    }
+
+    public String getQueue_image() {
+        return queue_image;
+    }
+
+    public void setQueue_image(String queue_image) {
+        this.queue_image = queue_image;
+    }
+
+    public String getQueue_city() {
+        return queue_city;
+    }
+
+    public void setQueue_city(String queue_city) {
+        this.queue_city = queue_city;
+    }
+
     public int getQueue_nMaxReservation() {
         return queue_nMaxReservation;
     }
 
+    public void setQueue_nMaxReservation(int queue_nMaxReservation) {
+        this.queue_nMaxReservation = queue_nMaxReservation;
+    }
+
+    public String getQueue_nReservationString() {
+        return Integer.toString(queue_nReservation);
+    }
+
     public int getQueue_nReservation() {
         return queue_nReservation;
+    }
+
+    public void setQueue_nReservation(int queue_nReservation) {
+        this.queue_nReservation = queue_nReservation;
     }
 }
