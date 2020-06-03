4d02dface84d8e9973aff57e61add3cb6d95bc3d
pietro <pietrovassallo04@gmail.com>
2020-06-03 11:16:29 +0200

    added QueueActivity

--- InstantReservation/app/src/main/java/com/example/instantreservation/Activity/QueueActivity.java
+++ InstantReservation/app/src/main/java/com/example/instantreservation/Activity/QueueActivity.java
@@ -1,9 +1,12 @@
 package com.example.instantreservation.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.example.instantreservation.R;
@@ -15,51 +18,35 @@ import com.google.firebase.database.ValueEventListener;
 
 public class QueueActivity extends AppCompatActivity {
 
-    final String queue_id;
-    final String queue_name;
-    final String queue_description;
-    final String queue_business;
-    final int queue_nMaxReservation;
-    final int queue_nReservation;
 
-    final FirebaseDatabase database = FirebaseDatabase.getInstance();
-
-    public QueueActivity(String queue_id, String queue_name, String queue_description, String queue_business, int queue_nMaxReservation, int queue_nReservation) {
-        this.queue_id = queue_id;
-        this.queue_name = queue_name;
-        this.queue_description = queue_description;
-        this.queue_business = queue_business;
-        this.queue_nMaxReservation = queue_nMaxReservation;
-        this.queue_nReservation = queue_nReservation;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_queue);
 
+        Toolbar toolbar = findViewById(R.id.toolbar_queue);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_back);
+        getSupportActionBar().setTitle(" QUEUE");
+
        // queue_id = findViewById(R.id.queue_id);
         Intent intent = getIntent();
         String id = intent.getStringExtra("payload");
-        //queue_id.setText(id);
 
-        DatabaseReference ref = database.getReference("instant-reservation-6c2a2/codeattivita/"+id);
-        // Attach a listener to read the data at our posts reference
-        ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                QueueActivity queue = dataSnapshot.getValue(QueueActivity.class);
-                System.out.println(queue.queue_name);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                System.out.println("The read failed: " + databaseError.getCode());
-            }
-        });
     }
 
-
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 
 }
