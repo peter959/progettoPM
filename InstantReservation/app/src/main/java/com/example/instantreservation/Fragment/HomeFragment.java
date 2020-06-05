f22a2ef691c3054047fbd448aa23b4bf0a57e673
Gioele <gcageggi@gmail.com>
2020-06-05 11:49:20 +0200

    Catching data for reservedQueue in Home

--- InstantReservation/app/src/main/java/com/example/instantreservation/Fragment/HomeFragment.java
+++ InstantReservation/app/src/main/java/com/example/instantreservation/Fragment/HomeFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
@@ -15,6 +16,11 @@ import android.widget.TextView;
 import com.example.instantreservation.QueueAdapter;
 import com.example.instantreservation.Queue;
 import com.example.instantreservation.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,12 +32,18 @@ import java.util.List;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
+
     private SharedPreferences userInfo;
     String name;
+    String userUID;
 
     ViewPager viewPager;
     QueueAdapter queueAdapter;
     List<Queue> models;
+
+    DatabaseReference referenceForReservedQueue;
+    DatabaseReference referenceForQueueInfo;
+
 
     private TextView hello_name;
 
@@ -50,33 +62,69 @@ public class HomeFragment extends Fragment {
 
         userInfo = getActivity().getSharedPreferences("UserInfo", Context.MODE_PRIVATE);
         name = userInfo.getString("userName" , "null");
+        userUID = userInfo.getString("userUID", "null");
+
+        referenceForReservedQueue = FirebaseDatabase.getInstance().getReference().child("users").child(userUID).child("reservedQueue");
+        referenceForQueueInfo = FirebaseDatabase.getInstance().getReference().child("codeattivita");
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View returnView = inflater.inflate(R.layout.fragment_home, container, false);
+        final View returnView = inflater.inflate(R.layout.fragment_home, container, false);
 
+        models = new ArrayList<>();
+        referenceForReservedQueue.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull final DataSnapshot dataSnapshot) {
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    final String queueID =  dataSnapshot1.getKey();
+                    System.out.println(dataSnapshot1.getKey());
+                    if (queueID!=null) {
+                        referenceForQueueInfo.child(queueID);
+                        referenceForQueueInfo.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot2) {
+                                Queue queue = dataSnapshot2.child(queueID).getValue(Queue.class);
+                                System.out.println(queue.getQueue_name());
+                                models.add(queue);
 
+                                queueAdapter = new QueueAdapter(models, HomeFragment.this);
 
+                                viewPager = returnView.findViewById(R.id.viewPager);
+                                viewPager.setAdapter(queueAdapter);
+                                queueAdapter.notifyDataSetChanged();
+                                viewPager.setPadding(0,0,80,0);
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         // Inflate the layout for this fragment
         TextView hello_name = (TextView) returnView.findViewById(R.id.hello_name);
         hello_name.setText("Hello " + name + "!");
 
-        models = new ArrayList<>();
-        models.add(new Queue("name", "description", "business", "businessID", "QRcode", "image", "city", 5,2));
-        models.add(new Queue("name", "description", "business", "businessID", "QRcode", "image", "city", 5,2));
-        models.add(new Queue("name", "description", "business", "businessID", "QRcode", "image", "city", 5,2));
-        models.add(new Queue("name", "description", "business", "businessID", "QRcode", "image", "city", 5,2));
+
+        //models.add(new Queue("name", "description", "business", "businessID", "QRcode", "image", "city", 5,2));
+       // models.add(new Queue("name", "description", "business", "businessID", "QRcode", "image", "city", 5,2));
+       // models.add(new Queue("name", "description", "business", "businessID", "QRcode", "image", "city", 5,2));
+        //models.add(new Queue("name", "description", "business", "businessID", "QRcode", "image", "city", 5,2));
 
 
-        queueAdapter = new QueueAdapter(models, this);
 
-        viewPager = returnView.findViewById(R.id.viewPager);
-        viewPager.setAdapter(queueAdapter);
-        viewPager.setPadding(0,0,80,0);
 
 
 
