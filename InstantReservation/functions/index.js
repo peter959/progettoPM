f1a5f765a52f207b1c0e4ead8471173fb3121206
Gioele <gcageggi@gmail.com>
2020-06-08 12:23:03 +0200

    Update index.js

--- InstantReservation/functions/index.js
+++ InstantReservation/functions/index.js
@@ -12,15 +12,47 @@ const client = algoliasearch(functions.config().algolia.algolia_app_id, function
 // Name fo the algolia index for Blog posts content.
 const ALGOLIA_QUEUES_INDEX_NAME = 'queues';
 
+exports.dbWrite = functions.database.ref('/queues/{queue_id}').onWrite((change, context) => {
+  //const index = client.initIndex(ALGOLIA_QUEUES_INDEX_NAME);
+  
+  const afterData = change.after.val(); // data after the write
+
+  const data = {
+  	objectID: context.params.queue_id,
+    queue_business: afterData.queue_business,
+    queue_city: afterData.queue_city
+  };
+
+  return addToAlgolia(data, 'queues')
+ 	.then(res => console.log('SUCCESS ALGOLIA equipment ADD', res))
+ 	.catch(err => console.log('ERROR ALGOLIA equipment ADD', err));
+
+
+});
+
+function addToAlgolia(object, indexName) {
+ console.log('GETS IN addToAlgolia')
+ console.log('object', object)
+ console.log('indexName', indexName)
+ const ALGOLIA_ID = functions.config().algolia.algolia_app_id;
+ const ALGOLIA_ADMIN_KEY = functions.config().algolia.algolia_api_key;
+ const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
+ const index = client.initIndex(indexName);
+return new Promise((resolve, reject) => {
+  index.saveObject(object)
+  });
+}
+
 // Updates the search index when new blog entries are created or updated.
-exports.indexentry = functions.database.ref('/queues/{queues_id}').onWrite(event => {
+/*exports.indexentry = functions.database.ref('/queues/{queues_id}').onWrite(event => {
   const index = client.initIndex(ALGOLIA_QUEUES_INDEX_NAME);
+  const afterData = event.data.val();
   const firebaseObject = {
-    queue_business: event.data.queue_business,
-    queue_city: event.data.queue_city
+    queue_business: afterData.data.queue_business,
+    queue_city: afterData.data.queue_city
   };
 
   return index.saveObject(firebaseObject).then(
       () => event.data.adminRef.parent.child('last_index_timestamp').set(
           Date.parse(event.timestamp)));
-});
+});*/
